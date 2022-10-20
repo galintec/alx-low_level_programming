6d1df9d26952593fd1f4feda470b6449d3d893a7
galintec <peemachy@gmail.com>
2022-10-20 09:09:46 -0700

    TAsk 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - main file is a test for a postitive_or_negative
+ * return: zero
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+
+	positive_or_negative(i);
+	return (0); }
