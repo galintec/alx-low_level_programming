5bc6462f32f0a83c838b048914763a2215034741
galintec <peemachy@gmail.com>
2022-10-28 09:06:39 -0700

    First commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,9 +10,9 @@ char *_strcat(char *dest, char *src)
 	int i, len;
 
 	for (len = 0; dest[len] != '\0'; len++);
-  
+	
 	for (i = 0; src[i] != '\0'; i++)
-		dest[len + i] = src[i];
+	dest[len + i] = src[i];
 	dest[len + i] = '\0';
 	return (dest);
 }
